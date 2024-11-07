ae752f53dd9e55e69d3f420bdfd66c10abc21632
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-11-07 20:14:24 +0900

    style: interceptor 주석추가

--- src/app/api/_interceptor/authApi/pendingRequestQueue.ts
+++ src/app/api/_interceptor/authApi/pendingRequestQueue.ts
@@ -1,5 +1,9 @@
 export type PendingRequest = (error: Error | null) => Promise<void>;
+
+// 사용자별 요청 대기 큐 - key(사용자 아이디)-value(요청대기 큐) Map
 export const pendingRequests: Map<string, PendingRequest[]> = new Map<string, PendingRequest[]>();
+
+// 사용자의 요청 대기 큐에 요청 추가
 export const addToPendingRequest = (userId: string, request: PendingRequest) => {
     const queue = pendingRequests.get(userId);
     if (queue === undefined) {
@@ -8,9 +12,13 @@ export const addToPendingRequest = (userId: string, request: PendingRequest) => 
         queue.push(request);
     }
 }
+
+// 사용자의 요청 대기 큐 삭제
 export const deleteFromPendingRequest = (userId: string) => {
     pendingRequests.delete(userId);
 }
+
+// 사용자의 요청 대기 큐 수행
 export const processPendingRequest = (userId: string, error: Error | null) => {
     const queue = pendingRequests.get(userId);
     if (queue) {
