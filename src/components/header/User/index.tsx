e89496472a56fab888a9c2a2981897fc12b9e060
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-11-19 22:59:32 +0900

    refactor: 안쓰는 코드제거

--- src/components/header/User/index.tsx
+++ src/components/header/User/index.tsx
@@ -1,5 +1,4 @@
 import UserMenu from "@/components/header/User/UserMenu";
 import UserMenuDropdown from "@/components/header/User/UserMenuDropdown";
-import User from "@/components/header/User/User";
 
-export {UserMenu, UserMenuDropdown, User};
+export {UserMenu, UserMenuDropdown};
