f57b5a4f1382c6ce07db7eecc359589e764499ac
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-12-05 20:48:43 +0900

    fix: sitemap 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,20 +5,16 @@ import {PageResponseBody, PostCardInfo} from "@/utils/type";
 
 const DOMAIN = process.env.NEXT_PUBLIC_URL;
 export default async function sitemap():Promise<MetadataRoute.Sitemap> {
-    //     const {techStacks, position, keyword, page} = DEFAULT_SEARCH_POST_PARAM;
-    // PageResponseBody<PostCardInfo[]>
-    //     await queryClient.prefetchQuery({
-    //         queryKey: ['postList', techStacks, position, keyword, page],
-    //         queryFn: () => getPostList(DEFAULT_SEARCH_POST_PARAM),
-    //     });
-
     try{
         const res:PageResponseBody<PostCardInfo[]> = await getPostList(DEFAULT_SEARCH_POST_PARAM);
-        return res.data.content.map((post) => ({
+        const postSiteMap:MetadataRoute.Sitemap = res.data.content.map((post) => ({
             url:`${DOMAIN}/post?postId=${post.boardId}&amp;projectId=${post.project.projectId}`,
             lastModified: new Date(post.updateDate),
             priority: 0.8
         }));
+
+        return [{url: `${DOMAIN}/`, lastModified: new Date(), priority: 1}, ...postSiteMap];
+
     }catch (e:unknown){
         console.error(e as Error);
         throw e as Error;
