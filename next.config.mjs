6f9b629923dc00991ad934dc9143948bcff708bf
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-24 20:26:03 +0900

    chore: cors설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -51,7 +51,7 @@ export default (phase, {defaultConfig}) => {
                { source: "/api/:path",
                    headers:[
                        { key: "Access-Control-Allow-Credentials", value: "true" },
-                       { key: "Access-Control-Allow-Origin", value: "www.trustcrews.com, trustcrews.com" },
+                       { key: "Access-Control-Allow-Origin", value: "www.trustcrews.com, trustcrews-front-forked-kim-eun-seons-projects.vercel.app" },
                        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
                        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                    ]
