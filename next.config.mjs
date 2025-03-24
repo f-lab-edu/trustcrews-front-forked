76b4b02ace8f9ef25639e7dfc938473ccf103f73
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-24 21:50:03 +0900

    chore: cors설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -46,17 +46,39 @@ export default (phase, {defaultConfig}) => {
                 }
             ]
         },
-        async headers(){
-           return [
-               { source: "/api/:path*",
-                   headers:[
-                       { key: "Access-Control-Allow-Credentials", value: "true" },
-                       { key: "Access-Control-Allow-Origin", value: "www.trustcrews.com, trustcrews-front-forked-kim-eun-seons-projects.vercel.app" },
-                       { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-                       { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                   ]
-               }
-           ]
+        async headers() {
+            return [
+                {
+                    source: "/api/project/:path*",
+                    headers: [
+                        {key: "Access-Control-Allow-Credentials", value: "true"},
+                        {
+                            key: "Access-Control-Allow-Origin",
+                            value: "www.trustcrews.com, trustcrews-front-forked-kim-eun-seons-projects.vercel.app"
+                        },
+                        {key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"},
+                        {
+                            key: "Access-Control-Allow-Headers",
+                            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                        },
+                    ]
+                },
+                {
+                    source: "/api/user/:path((?!login$|signup$)[a-zA-Z0-9_-]+)",
+                    headers: [
+                        {key: "Access-Control-Allow-Credentials", value: "true"},
+                        {
+                            key: "Access-Control-Allow-Origin",
+                            value: "www.trustcrews.com, trustcrews-front-forked-kim-eun-seons-projects.vercel.app"
+                        },
+                        {key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"},
+                        {
+                            key: "Access-Control-Allow-Headers",
+                            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                        },
+                    ]
+                }
+            ]
 
         }
     }
