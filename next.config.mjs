01d597ac439562a1d23f6ae257706043734b10c8
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-24 21:54:36 +0900

    chore: cors설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -49,6 +49,21 @@ export default (phase, {defaultConfig}) => {
         async headers() {
             return [
                 {
+                    source: "/api/:path*",
+                    headers: [
+                        {
+                            key: "Access-Control-Allow-Origin",
+                            value: "*"
+                        },
+                        {key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"},
+                        {
+                            key: "Access-Control-Allow-Headers",
+                            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                        },
+                    ]
+
+                },
+                {
                     source: "/api/project/:path*",
                     headers: [
                         {key: "Access-Control-Allow-Credentials", value: "true"},
